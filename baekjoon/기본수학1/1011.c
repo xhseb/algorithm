a8199e6e1f31b772356aa7caf5492f32126b3507
xhseb <zzzu5280@gmail.com>
2021-10-09 16:59:48 +0900

    Solved : #1011
    거리에 따른 장치 사용 횟수를 출력하는 문제
    - 제곱수를 기준으로 나누고, 나누어진 구역 내에서 규칙을 찾음

--- baekjoon/기본수학1/1011.c
+++ baekjoon/기본수학1/1011.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
+#include <math.h>
 
 int		main(void)
 {
-	int		T;
-	int		x;
-	int		y;
-	int		dis;
-	int		move;
+	int			T;
+	int			x;
+	int			y;
+	int			i;
+	long long	res;
 
-	scanf("%d",&T);
-	while (T--)
+	scanf("%d", &T);
+	i = -1;
+	while (++i < T)
 	{
 		scanf("%d %d", &x, &y);
-		dis = y - x;
-		if (dis == 1)
-			printf("1\n");
-		move = 1;
-		while (dis - move * 2 > 0)
+		if (y - x == 1)
 		{
-			dis = dis - move * 2;
-			move++;
+			printf("1\n");
+			continue ;
 		}
-		if (move >= dis)
-			printf("%d\n", move + 1);
-		else if (move * 2 >= dis)
-			printf("%d\n", move + 2);
-		//다른 조건이 더 필요.. 생각해보기
+		long long k;
+		k = (int)sqrt(y - x);
+		if (y - x == k * k)
+			res = 2 * k - 1;
+		else if (y - x > k * k + k)
+			res = 2 * k + 1;
+		else
+			res = 2 * k;
+		printf("%lld\n", res);
 	}
 	return (0);
 }
