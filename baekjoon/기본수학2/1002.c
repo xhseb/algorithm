8a9c6f165a35137aabd495e7ec0e614a209df92e
xhseb <zzzu5280@gmail.com>
2021-10-10 13:48:54 +0900

    Test : #1002

---
+++ baekjoon/기본수학2/1002.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int		main(void)
+{
+	int		T;
+	int		x1, x2, y1, y2;
+	int		r1, r2;
+	double	len;
+
+	len = 0;
+	scanf("%d", &T);
+	while (T--)
+	{
+		scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
+		len = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
+		if (len == 0 && r1 == r2)
+			printf("-1\n");
+		else if (abs(r1 - r2) > len || r1 + r1 > len)
+			printf("0\n");
+		else if (abs(r1 - r2) == len || len == r1 + r2)
+			printf("1\n");
+		else
+			printf("2\n");
+	}
+	return (0);
+}
