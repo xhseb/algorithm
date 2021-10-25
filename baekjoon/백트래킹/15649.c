7dfe1b4f1d947946795fecc89c63593b81e2e5b9
xhseb <zzzu5280@gmail.com>
2021-10-25 23:40:46 +0900

    Feat : 15649

---
+++ baekjoon/백트래킹/15649.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void    func(int max, int i, int dep, int j)
+{
+    if ()
+}
+
+int     main(void)
+{
+    int     max, dep;
+    int     arr[8] = {0, };
+
+    scanf("%d %d", &max, &dep);
+    func(max, 1, dep, 0);
+}
